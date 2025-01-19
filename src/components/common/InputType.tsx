7f2f5c86d68820cd9ff08c871ac91254e270ae3f
shivam <shivp72019@gmail.com>
2025-01-19 14:53:03 +0530

    password show logic implemented

--- src/components/common/InputType.tsx
+++ src/components/common/InputType.tsx
@@ -1,11 +1,17 @@
-import { LucideProps } from 'lucide-react'
-import React from 'react'
+import { Eye, EyeOff, LucideProps } from 'lucide-react'
+import React, { useState } from 'react'
 
 interface InputTypeProps {
     Icon?: React.ForwardRefExoticComponent<Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>>
 }
 
 export default function InputType({ Icon, ...props }: InputTypeProps & React.InputHTMLAttributes<HTMLInputElement>) {
+    const [showPassword, setShowPassword] = useState(false)
+
+    const togglePasswordShow = () => {
+        setShowPassword(!showPassword)
+    }
+
     return (
         <div className="relative">
             {Icon && <Icon className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5" style={{ color: '#8CB9C7' }} />}
@@ -17,7 +23,16 @@ export default function InputType({ Icon, ...props }: InputTypeProps & React.Inp
                     color: '#B8D8E1',
                     caretColor: '#B8D8E1'
                 }}
+                type={props.type === 'password' ? (showPassword ? 'text' : 'password') : props.type}
             />
+            {props.type === 'password' && (
+                <div className='absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer select-none' onClick={togglePasswordShow}>
+                    { showPassword
+                      ? <Eye className='w-5 h-5' style={{ color: '#8CB9C7' }} />
+                      : <EyeOff className='w-5 h-5' style={{ color: '#8CB9C7' }} />
+                    }
+                </div>
+            )}
         </div>
     )
 }
