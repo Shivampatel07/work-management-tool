5cf6c1ff275770727298b1630057b0f90eb21954
shivam <shivp72019@gmail.com>
2025-01-19 17:56:51 +0530

    User register is done

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,12 @@
+import { Metadata } from 'next'
 import React from 'react'
+import RegisterPage from './RegisterPage'
 
-export default function RegisterPage() {
-  return (
-    <div>page</div>
-  )
+export const metadata: Metadata = {
+    title: 'Register | Discusync',
+    description: 'We are excited to have you on board',
+}
+
+export default function page() {
+  return <RegisterPage />
 }
--- src/store/types.d.ts
+++ src/store/types.d.ts
@@ -4,6 +4,7 @@ export interface AuthState {
     token: string | null
     fetchProfile: () => Promise<boolean>
     login: (email: string, password: string) => Promise<void>
+    register: (email: string, password: string) => Promise<void>
 }
 
 export interface UserState {
