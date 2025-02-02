8187d0345ae8482a2e41d4c947aa86db2375db9e
shivam <shivp72019@gmail.com>
2025-02-02 15:16:16 +0530

    user model updated

--- src/lib/models/user.model.ts
+++ src/lib/models/user.model.ts
@@ -4,6 +4,12 @@ export interface IUser extends mongoose.Document {
   email: string;
   password: string;
   name: string;
+  role: string;
+  profile_picture?: string;
+  status: string;
+  is_online: boolean;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const UserSchema = new mongoose.Schema({
@@ -24,6 +30,16 @@ const UserSchema = new mongoose.Schema({
     type: String,
     default: "user"
   },
+  profile_picture: String,
+  status: {
+    type: String,
+    enum:  ["online", "offline", "busy", "away"],
+    default: "offline"
+  },
+  is_online: {
+    type: Boolean,
+    default: false
+  }
 }, { timestamps: true });
 
 
